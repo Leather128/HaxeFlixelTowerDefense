b9e830f76136af4a048b21793e80de8c9f492eb7
Leather128 <gwr.yt2@gmail.com>
2021-05-19 21:04:19 -0400

    Added a StartState.hx for the start menu (also updated Project.xml for HTML5 fullscreen stuff)

---
+++ source/StartState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class StartState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
